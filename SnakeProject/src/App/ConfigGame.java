d6d5587aa40693a88edd0813727cc42e91da62ac
LucasAguiar <lucas.aguiar.ads@gmail.com>
2023-12-17 18:45:04 -0300

    test: movimentação

--- SnakeProject/src/App/ConfigGame.java
+++ SnakeProject/src/App/ConfigGame.java
@@ -13,8 +13,8 @@ public class ConfigGame extends JPanel implements ActionListener{
 	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/(UNIT_SIZE*UNIT_SIZE);
 	static final int DELAY = 75;
 	final int x[] = new int[GAME_UNITS];
-	final int t[] = new int [GAME_UNITS];
-	int bodyParts = 3;
+	final int y[] = new int [GAME_UNITS];
+	int bodyParts = 4;
 	int appleEatens;
 	int appleX;
 	int appleY;
@@ -58,6 +58,16 @@ public class ConfigGame extends JPanel implements ActionListener{
 			g.setColor(new Color(239, 154, 154));
 			g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 			
+			for(int i = 0; i < bodyParts; i++) {
+				if(i == 0) {
+					g.setColor(new Color(129, 199, 132));
+					g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+				} else {
+					g.setColor(new Color(46, 125, 50));
+					g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+				}
+			}
+			
 		}
 		
 	}
@@ -68,6 +78,26 @@ public class ConfigGame extends JPanel implements ActionListener{
 	}
 	
 	public void move() {
+		
+		for(int i = bodyParts; i > 0; i--) {
+			x[i] = x[i-1];
+			y[i] = y[i-1];
+		}
+		
+		switch(direction){
+		case 'U':
+			y[0] = y[0] - UNIT_SIZE;
+			break;
+		case 'D':
+			y[0] = y[0] + UNIT_SIZE;
+			break;
+		case 'L':
+			x[0] = x[0] - UNIT_SIZE;
+			break;
+		case 'R':
+			x[0] = x[0] + UNIT_SIZE;
+			break;
+		}
 		
 	}
 	
@@ -86,6 +116,11 @@ public class ConfigGame extends JPanel implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
+		
+		if(running) {
+			move();
+		}
+		repaint();
 	}
 }
 
